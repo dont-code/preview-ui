b8a668897516de9556adab300a13592a62d042ca
Gérard Collin <gerard.collin@allianz.com>
2020-04-13 18:56:00 +0200

    Basic Preview

---
+++ apps/preview-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'preview-ui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'preview-ui';
+}
