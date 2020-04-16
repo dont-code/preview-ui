1d0682cad75b03665f865e9ada8c4c2da485f239
Gérard Collin <gerard.collin@allianz.com>
2020-04-16 11:03:43 +0200

    missing e2e files

--- apps/preview-ui-e2e/src/integration/app.spec.ts
+++ apps/preview-ui-e2e/src/integration/app.spec.ts
@@ -4,8 +4,6 @@ describe('preview-ui', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display standard layout', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
 
     getToolbar();
     getToolbarTitle().contains('Preview');
---
+++ apps/preview-ui-e2e/src/integration/command.spec.ts
@@ -0,0 +1,29 @@
+import { getToolbarTitle } from '../support/app.po';
+import { MainComponent } from '../../../preview-ui/src/app/layout/main/main.component';
+import { getMenuForEntity } from '../support/command.po';
+
+describe('commands', () => {
+  beforeEach(() => {
+    /*cy.getService(CommandProviderService).then ((service:CommandProviderService) => {
+      console.log('stubbing');
+      cy.stub(service, 'receiveCommands', () => {
+        console.log('stubbed');
+      });
+    });*/
+    cy.visit('/');
+  });
+
+  it('should display updated name', () => {
+
+    getToolbarTitle().contains('New Name');
+    cy.findNgComponent('preview-ui-main').then((comp: MainComponent) => {
+      comp.appName = 'Testing Name';
+      cy.applyChanges(comp);
+      getToolbarTitle().contains('Testing Name');
+    });
+  });
+  it('should display entities menu', () => {
+    getMenuForEntity('ToDoItem');
+    getMenuForEntity('Country');
+  });
+});
