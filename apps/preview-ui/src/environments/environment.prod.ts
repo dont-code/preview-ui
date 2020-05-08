9a79b6ed697bdd67cd7e5395d81ba6bf33ea6697
Gérard Collin <gerard.collin@allianz.com>
2020-05-08 15:12:19 +0200

    Fixed service url

--- apps/preview-ui/src/environments/environment.prod.ts
+++ apps/preview-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  webSocketUrl: 'wss://prv-services-demo.apps.us-west-1.starter.openshift-online.com/ide'
+  webSocketUrl: 'wss://prv-services-demo.apps.us-west-1.starter.openshift-online.com/preview'
 };
