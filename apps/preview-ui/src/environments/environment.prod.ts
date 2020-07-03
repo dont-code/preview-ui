91eade895b88fa8179d7082293fbd56fbe17c030
Gérard Collin <gerard.collin@appliweb.net>
2020-07-03 17:01:17 +0200

    New url

--- apps/preview-ui/src/environments/environment.prod.ts
+++ apps/preview-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  webSocketUrl: 'wss://prv-services-demo.apps.us-west-1.starter.openshift-online.com/preview'
+  webSocketUrl: 'https://preview-services-demo.apps.us-west-1.starter.openshift-online.com/preview'
 };
