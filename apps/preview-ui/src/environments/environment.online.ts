0c92ba08946efeae58cb5c5c6c320560c70ee998
Gérard Collin <gerard.collin@allianz.com>
2020-09-05 10:44:37 +0200

    Updated service url

--- apps/preview-ui/src/environments/environment.online.ts
+++ apps/preview-ui/src/environments/environment.online.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  webSocketUrl: 'wss://preview-services-demo.apps.us-west-1.starter.openshift-online.com/preview'
+  webSocketUrl: 'wss://preview-services-dontcode-demo.apps.us-west-1.starter.openshift-online.com/preview'
 };
