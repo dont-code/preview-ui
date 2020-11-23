a518156ec94ba520abb7a8ede640d0054d767a14
Gérard Collin <gerard.collin@allianz.com>
2020-11-23 14:53:29 +0100

    Fixed unit tests

--- apps/preview-ui/src/app/shared/dev/services/dev-template-manager.service.spec.ts
+++ apps/preview-ui/src/app/shared/dev/services/dev-template-manager.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DevTemplateManagerService } from './dev-template-manager.service';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe('DevTemplateManagerService', () => {
   let service: DevTemplateManagerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(DevTemplateManagerService);
   });
 
