021b0241cd0d7856f3bc9aff60db6d9aef718a0d
Gérard Collin <gerard.collin@allianz.com>
2020-12-31 11:15:43 +0100

    Enhanced updates

--- apps/preview-ui/src/app/shared/values/services/value.service.spec.ts
+++ apps/preview-ui/src/app/shared/values/services/value.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ValueService } from './value.service';
+import { Change, ChangeType, DontCodeModelPointer } from "@dontcode/core";
+import { Subject } from "rxjs";
 
 describe('ValueService', () => {
   let service: ValueService;
@@ -55,4 +57,124 @@ describe('ValueService', () => {
     expect (service.findAtPosition('creation/entities/aaaa/fields/aaab')).toHaveProperty('name', 'Field1');
     expect (service.findAtPosition('creation/screens')).toHaveProperty('aaae', {"name":"Screen1"});
   });
+
+  it('should update content correctly from commands', () => {
+    service.resetContent ({
+      });
+    let source = new Subject<Change> ();
+    service.receiveUpdatesFrom(source);
+    source.next(createTestChange("creation",null,null, null,"TestName", "name"));
+    expect (service.getContent()).toEqual({
+      creation: {
+        name:"TestName"
+        }
+      });
+    source.next(createTestChange("creation",null,null, null,"TestApp", "type"));
+    expect (service.getContent()).toEqual({
+      creation: {
+        name:"TestName",
+        type:"TestApp"
+        }
+      });
+
+    source.next(createTestChange("creation/entities",'a',null, null,"TestEntity", "name"));
+    expect (service.getContent()).toEqual({
+      creation: {
+        name:"TestName",
+        type:"TestApp",
+        entities: {
+          "a": {
+            name:"TestEntity"
+          }
+        }
+      }
+    });
+
+    service.resetContent ({
+      creation: {
+        name: "TestName"
+      }
+    });
+    source.next(createTestChange("creation",null,'entities', 'b',
+      { name:"TestEntity", type:"string" }));
+    expect (service.getContent()).toEqual({
+      creation: {
+        name:"TestName",
+        entities: {
+          "b": {
+            name:"TestEntity",
+            type:"string"
+          }
+        }
+      }
+    });
+
+    source.next(createTestChange("creation",null, "entities","b", "number", "type"));
+    expect (service.getContent()).toEqual({
+      creation: {
+        name:"TestName",
+        entities: {
+          "b": {
+            name:"TestEntity",
+            type:"number"
+          }
+        }
+      }
+    });
+
+    service.resetContent ({
+      creation: {
+        name: "TestName"
+      }
+    });
+    source.next(createTestChange("creation/screens",'ab', "components","cd", "Search", "name"));
+    expect (service.getContent()).toEqual({
+      creation: {
+        name:"TestName",
+        screens: {
+          "ab": {
+            components:{
+              "cd": {
+                name:"Search"
+              }
+            }
+          }
+        }
+      }
+    });
+
+  });
+
+  function createTestChange(containerSchema: string, containerItemId: string, schema: string, itemId: string, value: any, property?:string) {
+    let calcContainerItemId=containerItemId?'/'+containerItemId:'';
+    let calcItemId=itemId?'/'+itemId:'';
+    let calcSchema=schema?'/'+schema:'';
+
+    if( property) {
+      return new Change(ChangeType.ADD,
+        containerSchema + calcContainerItemId + calcSchema + calcItemId + '/' + property,
+        value, new DontCodeModelPointer(
+          containerSchema + calcContainerItemId + calcSchema + calcItemId + '/' + property,
+          containerSchema + calcSchema+'/'+property,
+          containerSchema + calcContainerItemId+calcSchema + calcItemId,
+          containerSchema + calcSchema,
+          property,
+          null
+        ));
+    } else {
+      return new Change(ChangeType.ADD,
+        containerSchema + calcContainerItemId + calcSchema + calcItemId,
+        value, new DontCodeModelPointer(
+          containerSchema + calcContainerItemId + calcSchema + calcItemId,
+          containerSchema + calcSchema,
+          containerSchema + calcContainerItemId,
+          containerSchema,
+          null,
+          itemId
+        ));
+
+    }
+  };
+
+
 });
