6e1e906afad94cc9ea770d211aa7d34038283da4
Gérard Collin <gerard.collin@allianz.com>
2021-01-06 11:00:48 +0100

    Move value is supported by ValueService

--- apps/preview-ui/src/app/shared/values/services/value.service.spec.ts
+++ apps/preview-ui/src/app/shared/values/services/value.service.spec.ts
@@ -59,122 +59,223 @@ describe('ValueService', () => {
   });
 
   it('should update content correctly from commands', () => {
-    service.resetContent ({
-      });
-    let source = new Subject<Change> ();
+    service.resetContent({});
+    let source = new Subject<Change>();
     service.receiveUpdatesFrom(source);
-    source.next(createTestChange("creation",null,null, null,"TestName", "name"));
-    expect (service.getContent()).toEqual({
+    source.next(createTestChange("creation", null, null, null, "TestName", "name"));
+    expect(service.getContent()).toEqual({
       creation: {
-        name:"TestName"
-        }
-      });
-    source.next(createTestChange("creation",null,null, null,"TestApp", "type"));
-    expect (service.getContent()).toEqual({
+        name: "TestName"
+      }
+    });
+    source.next(createTestChange("creation", null, null, null, "TestApp", "type"));
+    expect(service.getContent()).toEqual({
       creation: {
-        name:"TestName",
-        type:"TestApp"
-        }
-      });
+        name: "TestName",
+        type: "TestApp"
+      }
+    });
 
-    source.next(createTestChange("creation/entities",'a',null, null,"TestEntity", "name"));
-    expect (service.getContent()).toEqual({
+    source.next(createTestChange("creation/entities", 'a', null, null, "TestEntity", "name"));
+    expect(service.getContent()).toEqual({
       creation: {
-        name:"TestName",
-        type:"TestApp",
+        name: "TestName",
+        type: "TestApp",
         entities: {
           "a": {
-            name:"TestEntity"
+            name: "TestEntity"
           }
         }
       }
     });
 
-    service.resetContent ({
+    service.resetContent({
       creation: {
         name: "TestName"
       }
     });
-    source.next(createTestChange("creation",null,'entities', 'b',
-      { name:"TestEntity", type:"string" }));
-    expect (service.getContent()).toEqual({
+    source.next(createTestChange("creation", null, 'entities', 'b',
+      {name: "TestEntity", type: "string"}));
+    expect(service.getContent()).toEqual({
       creation: {
-        name:"TestName",
+        name: "TestName",
         entities: {
           "b": {
-            name:"TestEntity",
-            type:"string"
+            name: "TestEntity",
+            type: "string"
           }
         }
       }
     });
 
-    source.next(createTestChange("creation",null, "entities","b", "number", "type"));
-    expect (service.getContent()).toEqual({
+    source.next(createTestChange("creation", null, "entities", "b", "number", "type"));
+    expect(service.getContent()).toEqual({
       creation: {
-        name:"TestName",
+        name: "TestName",
         entities: {
           "b": {
-            name:"TestEntity",
-            type:"number"
+            name: "TestEntity",
+            type: "number"
           }
         }
       }
     });
 
-    service.resetContent ({
+    service.resetContent({
       creation: {
         name: "TestName"
       }
     });
-    source.next(createTestChange("creation/screens",'ab', "components","cd", "Search", "name"));
-    expect (service.getContent()).toEqual({
+    source.next(createTestChange("creation/screens", 'ab', "components", "cd", "Search", "name"));
+    expect(service.getContent()).toEqual({
       creation: {
-        name:"TestName",
+        name: "TestName",
         screens: {
           "ab": {
-            components:{
+            components: {
               "cd": {
-                name:"Search"
+                name: "Search"
               }
             }
           }
         }
       }
     });
+  });
+
+    it('should delete content correctly from commands', () => {
+      service.resetContent({
+        creation: {
+          name: "TestName",
+          type: "TestApp",
+          entities: {
+            "a": {
+              name: "TestEntityA",
+              type: "boolean"
+            },
+            "b": {
+              name: "TestEntityB",
+              type: "string"
+            }
+          }
+        }
+      });
+      let source = new Subject<Change>();
+      service.receiveUpdatesFrom(source);
+      source.next(createDeleteChange("creation", null, null, null, "type"));
+      expect(service.getContent()).toEqual({
+        creation: {
+          name: "TestName",
+          entities: {
+            "a": {
+              name: "TestEntityA",
+              type: "boolean"
+            },
+            "b": {
+              name: "TestEntityB",
+              type: "string"
+            }
+          }
+        }
+      });
+      source.next(createDeleteChange("creation", null, "entities", "a"));
+      expect(service.getContent()).toEqual({
+        creation: {
+          name: "TestName",
+          entities: {
+            "b": {
+              name: "TestEntityB",
+              type: "string"
+            }
+          }
+        }
+      });
+      source.next(createDeleteChange("creation", null, "entities", "b", "name"));
+      expect(service.getContent()).toEqual({
+        creation: {
+          name: "TestName",
+          entities: {
+            "b": {
+              type: "string"
+            }
+          }
+        }
+      });
+      source.next(createDeleteChange("creation", null, "entities", null,null));
+      expect(service.getContent()).toEqual({
+        creation: {
+          name: "TestName"
+        }
+      });
 
   });
 
+  it('should move content correctly from commands', () => {
+    service.resetContent({
+      creation: {
+        name: "TestName",
+        type: "TestApp",
+        entities: {
+          "a": {
+            name: "TestEntityA",
+            type: "boolean"
+          },
+          "b": {
+            name: "TestEntityB",
+            type: "string"
+          },
+          "c": {
+            name: "TestEntityC",
+            type: "numeric"
+          }
+        }
+      }
+    });
+    const start=Object.keys(service.getContent().creation.entities);
+    expect(start).toEqual(["a","b","c"]);
+    let source = new Subject<Change>();
+    service.receiveUpdatesFrom(source);
+    // from a,b,c to b,a,c
+    source.next(createMoveChange("creation/entities/b","a","creation", null, "entities", "b"));
+    expect(Object.keys(service.getContent().creation.entities)).toStrictEqual(["b","a","c"]);
+    // from b,a,c to b,c,a
+    source.next(createMoveChange("creation/entities/c","a","creation", null, "entities", "c"));
+    expect(Object.keys(service.getContent().creation.entities)).toStrictEqual(["b","c","a"]);
+    // from b,c,a to c,a,b
+    source.next(createMoveChange("creation/entities/b",null,"creation", null, "entities", "b"));
+    expect(Object.keys(service.getContent().creation.entities)).toStrictEqual(["c","a", "b"]);
+  });
+
+  function createDeleteChange (containerSchema: string, containerItemId: string, schema: string, itemId: string, property?:string) {
+    return createAnyChange(ChangeType.DELETE, containerSchema, containerItemId, schema, itemId, null, property);
+  }
+
+  function createMoveChange (oldPosition:string, beforeIdOrProperty:string, containerSchema: string, containerItemId: string, schema: string, itemId: string, property?:string) {
+    const ret = createAnyChange(ChangeType.MOVE, containerSchema, containerItemId, schema, itemId, null, property);
+    ret.oldPosition=oldPosition;
+    ret.beforeKey=beforeIdOrProperty;
+    return ret;
+  }
+
   function createTestChange(containerSchema: string, containerItemId: string, schema: string, itemId: string, value: any, property?:string) {
+    return createAnyChange(ChangeType.ADD, containerSchema, containerItemId, schema, itemId, value, property);
+  }
+  function createAnyChange(type:ChangeType, containerSchema: string, containerItemId: string, schema: string, itemId: string, value: any, property?:string) {
     let calcContainerItemId=containerItemId?'/'+containerItemId:'';
     let calcItemId=itemId?'/'+itemId:'';
     let calcSchema=schema?'/'+schema:'';
+    let calcProperty=property?'/'+property:'';
 
-    if( property) {
-      return new Change(ChangeType.ADD,
-        containerSchema + calcContainerItemId + calcSchema + calcItemId + '/' + property,
+      return new Change(type,
+        containerSchema + calcContainerItemId + calcSchema + calcItemId + calcProperty,
         value, new DontCodeModelPointer(
-          containerSchema + calcContainerItemId + calcSchema + calcItemId + '/' + property,
-          containerSchema + calcSchema+'/'+property,
-          containerSchema + calcContainerItemId+calcSchema + calcItemId,
-          containerSchema + calcSchema,
-          property,
-          null
+          containerSchema + calcContainerItemId + calcSchema + calcItemId + calcProperty,
+          containerSchema + calcSchema + calcProperty,
+          containerSchema + calcContainerItemId+ property?(calcSchema + calcItemId):'',
+          containerSchema + property?(calcSchema):'',
+          property?property:null,
+          property?null:itemId
         ));
-    } else {
-      return new Change(ChangeType.ADD,
-        containerSchema + calcContainerItemId + calcSchema + calcItemId,
-        value, new DontCodeModelPointer(
-          containerSchema + calcContainerItemId + calcSchema + calcItemId,
-          containerSchema + calcSchema,
-          containerSchema + calcContainerItemId,
-          containerSchema,
-          null,
-          itemId
-        ));
-
-    }
   };
-
 
 });
--- apps/preview-ui/src/app/shared/values/services/value.service.ts
+++ apps/preview-ui/src/app/shared/values/services/value.service.ts
@@ -1,4 +1,4 @@
-import { Change } from "@dontcode/core";
+import { Change, ChangeType } from "@dontcode/core";
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 
@@ -25,7 +25,38 @@ export class ValueService {
       const parentPos = change.position.substring (0, change.position.lastIndexOf ('/'));
       const prop = change.position.substring (change.position.lastIndexOf ('/')+1);
       const parent = this.findAtPosition (parentPos, true);
-      parent[prop]=change.value;
+      switch (change.type) {
+        case ChangeType.ADD:
+        case ChangeType.UPDATE:
+        case ChangeType.RESET:
+          parent[prop]=change.value;
+          break;
+        case ChangeType.DELETE:
+          delete parent[prop];
+          break;
+        case ChangeType.MOVE:
+          const oldParentPos = change.oldPosition.substring (0, change.oldPosition.lastIndexOf ('/'));
+          const oldProp = change.oldPosition.substring (change.oldPosition.lastIndexOf ('/')+1);
+          const oldParent = this.findAtPosition (oldParentPos, false);
+
+            // If needed, ensure the order of property is correct in target
+          const val = oldParent[oldProp];
+          delete oldParent[oldProp];
+          if (change.beforeKey) {
+            const keys=Object.keys(parent);
+            for (let key of keys) {
+              const copy=parent[key];
+              delete parent[key];
+              if (key===change.beforeKey) {
+                parent[prop]=val;
+              }
+              parent[key]=copy;
+            }
+          } else {
+            parent[prop]=val;
+          }
+          break;
+      }
     }
 
     )
