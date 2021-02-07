cfaaa2d54232c1eb7a2e3d05b1806cb7e6a522e2
Gérard Collin <gerard.collin@allianz.com>
2021-02-07 15:25:28 +0100

    Cypress tests updated

--- apps/preview-ui-e2e/src/integration/debug.spec.ts
+++ apps/preview-ui-e2e/src/integration/debug.spec.ts
@@ -1,10 +1,10 @@
 import {
-  clickAutoComplete, getHeaderMenu,
-  getSendButton,
-  getSubMenu,
+  clickAutoComplete, getHeaderMenu, getPageTitle,
+  getSendButton, getStep,
+  getSubMenu, getSubMenus, getSubMenuWithText,
   getToolbar,
   getValueTextArea,
-  selectPopupChoice
+  selectPopupChoice, selectPopupChoiceWithText, selectType
 } from "../support/app.po";
 import { MainComponent } from '../../../preview-ui/src/app/layout/main/main.component';
 
@@ -31,15 +31,101 @@ describe('debug', () => {
 
   });
   it('should add Entity Menu', () => {
-    getSubMenu(3).should ('contain.text', 'Dev');
-    getSubMenu(3).click();    // Move to dev page
+    getSubMenuWithText('Dev').click();// Move to dev page
 
     clickAutoComplete("template");
-    selectPopupChoice(2); // Create Entity name
-    getValueTextArea ().clear().type ("NewItem");
-    getSendButton ().click();
+    selectPopupChoiceWithText("Change Entity A Name"); // Create Entity name
+    getValueTextArea().clear().type("NewItem");
+    getSendButton().click();
     getHeaderMenu(4).should('contain.text', 'Application Menu');
-    getSubMenu(5).should('contain.text', 'NewItem');
-
+    getSubMenuWithText( 'NewItem').click();
+    getPageTitle().should('contain.text',"NewItem");
   });
+
+
+  it('should manage move and delete', () => {
+    getSubMenuWithText('Dev').click();// Move to dev page
+
+    // Creates A,B,C
+
+    clickAutoComplete("template");
+    selectPopupChoiceWithText("Change Entity A Name"); // Create Entity name
+    getValueTextArea().clear().type("A Name");
+    getSendButton().click();
+
+    clickAutoComplete("template");
+    selectPopupChoiceWithText("Create Entity A"); // Create Entity
+    getStep().clear().type("creation/entities/b")
+    getValueTextArea ().clear().type ("{{}\"name\":\"B Name\"}");
+    getSendButton ().click();
+
+    clickAutoComplete("template");
+    selectPopupChoiceWithText("Create Entity A"); // Create Entity
+    getStep().clear().type("creation/entities/c")
+    getValueTextArea ().clear().type ("{{}\"name\":\"C Name\"}");
+    getSendButton ().click();
+
+    assertEntityMenu ('A Name','B Name', 'C Name');
+
+      // Move B before A
+    clickAutoComplete("template");
+    selectPopupChoiceWithText("Move Entity b");
+    getSendButton ().click();
+
+    assertEntityMenu ('B Name','A Name', 'C Name');
+
+    // Move C before A
+    clickAutoComplete("template");
+    selectPopupChoiceWithText("Move Entity b");
+    getStep().clear().type("creation/entities/c")
+    getValueTextArea ().clear().type ("a");
+    getSendButton ().click();
+
+    assertEntityMenu ('B Name','C Name', 'A Name');
+
+    // Move B at the end
+    clickAutoComplete("template");
+    selectPopupChoiceWithText("Move Entity b");
+    getStep().clear().type("creation/entities/b")
+    getValueTextArea ().clear();
+    getSendButton ().click();
+
+    assertEntityMenu ('C Name', 'A Name','B Name');
+
+    // Delete B
+    clickAutoComplete("template");
+    selectPopupChoiceWithText("Move Entity b");
+    selectType("DELETE");
+    getValueTextArea ().clear();
+    getSendButton ().click();
+
+    assertEntityMenu ('C Name', 'A Name');
+    // Delete C
+    clickAutoComplete("template");
+    selectPopupChoiceWithText("Move Entity b");
+    getStep().clear().type("creation/entities/c")
+    selectType("DELETE");
+    getValueTextArea ().clear();
+    getSendButton ().click();
+
+    assertEntityMenu ('A Name');
+
+    // Delete A
+    clickAutoComplete("template");
+    selectPopupChoiceWithText("Move Entity b");
+    getStep().clear().type("creation/entities/a")
+    selectType("DELETE");
+    getValueTextArea ().clear();
+    getSendButton ().click();
+
+    getSubMenus().should("have.length", 4);
+  });
+
+  function assertEntityMenu(first: string, second?: string, third?: string) {
+    getSubMenu(5).should('contain.text', first);
+    if (second)
+      getSubMenu(6).should('contain.text', second);
+    if( third)
+      getSubMenu(7).should('contain.text', third);
+  }
 });
