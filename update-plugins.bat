dc07ea24e619a59e3b250f13f53e9dc369d6be37
gerard collin <gerard.collin@allianz.com>
2021-03-15 16:30:03 +0100

    Added fields plugin

--- update-plugins.bat
+++ update-plugins.bat
@@ -3,16 +3,6 @@ call nx run common:build --prod
 call nx run basic:build --prod
 call nx run screen:build --prod
 call nx run fields:build --prod
-call npm pack dist/libs/common
-call npm pack dist/libs/basic
-call npm pack dist/libs/screen
-call npm pack dist/libs/fields
 cd ..\preview-ui
-del dontcode-plugin-*.tgz
-move ..\plugins\dontcode-plugin-common-*.tgz .
-move ..\plugins\dontcode-plugin-basic-*.tgz .
-move ..\plugins\dontcode-plugin-screen-*.tgz .
-move ..\plugins\dontcode-plugin-fields-*.tgz .
-rem forfiles /M dontcode-plugin-*.tgz /C "cmd /c npm install @file"
-call npm install dontcode-plugin-common-1.31.tgz
-call npm install dontcode-plugin-basic-1.31.tgz
+call npm uninstall @dontcode/plugin-common @dontcode/plugin-basic @dontcode/plugin-screen @dontcode/plugin-fields
+call npm install "..\plugins\dist\libs\common" "..\plugins\dist\libs\basic" "..\plugins\dist\libs\screen" "..\plugins\dist\libs\fields"
