1fc052539cb30bf8a35bc7137d61977b7a085b67
Gérard Collin <gerard.collin@appliweb.net>
2024-04-07 17:25:12 +0200

    Changed urls of services

--- apps/preview-ui/src/environments/environment.prod.ts
+++ apps/preview-ui/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  webSocketUrl: 'wss://test.dont-code.net/preview',
+  webSocketUrl: 'wss://test.dont-code.net/demo/preview',
   repositoryUrl: 'assets/repositories/stable.json',  // To be switched to https://test.dont-code.net/data/Plugin%20Module whenever it is ready
-  storeApiUrl: 'https://test.dont-code.net/data',
-  projectApiUrl: 'https://test.dont-code.net/demo_project',
-  documentApiUrl: 'https://test.dont-code.net/documents',
+  storeApiUrl: 'https://test.dont-code.net/demo/data',
+  projectApiUrl: 'https://test.dont-code.net/demo/project',
+  documentApiUrl: 'https://test.dont-code.net/demo/documents',
 };
