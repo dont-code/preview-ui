8e3c6cc9d33f9458aaa9d2c68b502da6cf2d61d2
Gérard Collin <gerard.collin@appliweb.net>
2024-04-09 15:43:49 +0200

    Update repository urls

--- apps/preview-ui/src/environments/environment.prod.ts
+++ apps/preview-ui/src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
-  webSocketUrl: 'wss://test.dont-code.net/preview',
-  repositoryUrl: 'assets/repositories/stable.json',
-  storeApiUrl: 'https://test.dont-code.net/data',
-  projectApiUrl: 'https://test.dont-code.net/demo_project',
-  documentApiUrl: 'https://test.dont-code.net/documents',
+  //repositoryUrl: 'assets/repositories/stable.json',
+  repositoryUrl: 'https://test.dont-code.net/demo/repository-stable.json'
 };
